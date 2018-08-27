3cc9d6106c3c29ff28b339092b33c591cca25a33
Adrien Quillet <adrienquillet@gmail.com>
2018-08-27 15:58:27 +0200

    Tasks Controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :horses do
+    resources :tasks, only: [:new, :index, :create]
+  end
 end
