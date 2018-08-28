9581b36056f81fbecd0765ef6b4f31d48e84590c
Adrien Quillet <adrienquillet@gmail.com>
2018-08-28 18:03:20 +0200

    Mega formulaire crado du horse create

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -11,13 +11,16 @@ class HorsesController < ApplicationController
   end
 
   def new
+    @week_days = [ "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche", "toujours" ]
     @horse = Horse.new
   end
 
   def create
+    raise
     @horse = Horse.new(horse_params)
     @horse.user = current_user
     if @horse.save
+      create_tasks
       redirect_to horses_path
     else
       render :new
@@ -41,8 +44,16 @@ class HorsesController < ApplicationController
 
   private
 
+  def create_tasks
+    # on est dans le matin
+    # Task.new(
+        # food: Food.find_by(name: params[:foin]),
+        # food_quantity: params[:foin][:matin][:quantity]
+      # )
+  end
+
   def horse_params
-    params.require(:horse).permit(:name, :box, :formula)
+    params.require(:horse).permit(:name, :box, :formula, :user_id)
   end
 
   def set_horse
