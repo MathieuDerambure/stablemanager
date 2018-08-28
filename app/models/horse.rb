4793945375f592569c4bfe71d7901076efa2a2ed
Olivier Petre <opetrebe@gmail.com>
2018-08-28 11:27:48 +0200

    search_part1 + seed_part1

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,4 +1,12 @@
 class Horse < ApplicationRecord
   belongs_to :user
   has_many :tasks
+
+  include PgSearch
+    pg_search_scope :search_horses,
+      against: [ :name, :box, :formula ],
+      using: {
+        tsearch: { prefix: true }
+      }
+
 end
