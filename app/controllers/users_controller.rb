427ad962d0ce972ee24321ac1fd9ede8f27b52c4
Mathieu Derambure <deramburemathieu@hotmail.com>
2018-08-29 11:24:15 +0200

    première étape users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :destroy, :update]
+
+  def index
+    @users = User.all
+  end
+
+
+  def show
+  end
+
+
+
+
+  private
+  def user_params
+    params.require(:user).permit(:sur_name, :first_name, :role, :phone, :address, :email)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :horses do
     resources :tasks, only: [:new, :index, :create]
   end
+  resources :users
+
 end
