f4f8eda395792be1d88b3f403348e825c7eb0437
Mathieu Derambure <deramburemathieu@hotmail.com>
2018-08-29 12:58:45 +0200

    plutôt cool

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,15 +15,12 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:sur_name, :first_name, :role, :phone, :address, :email)
+    params.require(:user).permit(:sur_name, :first_name, :role, :phone, :address, :email, :photo)
   end
 
   def set_user
     @user = User.find(params[:id])
   end
-
-
-
 
 
 end
