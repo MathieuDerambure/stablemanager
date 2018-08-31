29f91576663d2100744d8819a3cb4fadce9df4d2
yann salmon <salomon912@hotmail.com>
2018-08-31 11:10:09 +0200

    show-modif

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -24,6 +24,7 @@ class HorsesController < ApplicationController
     @horse.user = current_user
     if @horse.save
       create_tasks
+      raise
       redirect_to horses_path
     else
       render :new
