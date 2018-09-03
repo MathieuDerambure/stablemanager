ab17eb7b267ebeb4b485d8476fcc01d5e510835b
Olivier Petre <opetrebe@gmail.com>
2018-09-03 16:05:06 +0200

    create_vaccin_job

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_31_131335) do
+ActiveRecord::Schema.define(version: 2018_09_03_134551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,7 +84,7 @@ ActiveRecord::Schema.define(version: 2018_08_31_131335) do
     t.bigint "activity_id"
     t.bigint "medecine_id"
     t.bigint "comment_id"
-    t.bigint "Task.where("vetenary_id |= nil")vetenary_id"
+    t.bigint "vetenary_id"
     t.bigint "shoe_maker_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 2018_08_31_131335) do
     t.string "sur_name"
     t.string "first_name"
     t.string "photo"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
