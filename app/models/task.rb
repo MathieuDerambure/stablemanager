44809f1732af522584275edef14ac75607013a09
Mathieu Derambure <deramburemathieu@hotmail.com>
2018-09-04 18:12:34 +0200

    cécile est amazing

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,6 +10,27 @@ class Task < ApplicationRecord
   belongs_to :antidote, optional: true
   belongs_to :user_doing, class_name: "User", optional: true
 
-  scope :not_done, -> { where.not(done: true) }
-  scope :due_today, -> { where("date(start_time) = ?", Date.today) }
+  # TYPE
+  scope :activity,   -> { where.not(activity_id: nil) }
+  scope :antidote,   -> { where.not(antidote_id: nil) }
+  scope :food,       -> { where.not(food_type_id: nil) }
+  scope :medecine,   -> { where.not(medecine_id: nil) }
+  scope :shoe_maker, -> { where.not(shoe_maker_id: nil) }
+  scope :vetenary,   -> { where.not(vetenary_id: nil) }
+
+  # STATUS
+  scope :not_done,   -> { where.not(done: true) }
+  scope :due_today,  -> { where("date(start_time) = :today", today: Date.today) }
+
+  # TIME
+  scope :to_come,    -> { where("date(start_time) >= :today", today: Date.today) }
+
+  scope :this_week,  -> do
+    where(
+      "date(start_time) BETWEEN :monday AND :sunday",
+      monday: Date.today.beginning_of_week,
+      sunday: Date.today.end_of_week
+    ).
+    order(:start_time)
+  end
 end
