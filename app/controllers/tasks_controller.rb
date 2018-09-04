0af073d3a9f122916f5954106cd918b74d9a3dd7
yann salmon <salomon912@hotmail.com>
2018-09-04 16:21:42 +0200

    push master

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,22 +3,14 @@ class TasksController < ApplicationController
   before_action :load_food_tasks, only: [:mark_as_doing_food, :mark_as_done_food]
 
   def tasks_index
-      @tasks = Task.select{|t| t.start_time == Date.today}
+      @tasks = Task.where(start_time: Date.today)
       @horses = Horse.all
 
       # Permet de compter les tasks de différents type pour affiche le nombre
       #dans la bulle rouge de la navbar
-      @tasks_food_type = @tasks.map{|task| task if task.food_type}.compact
-      @tasks_activity = @tasks.map{|task| task if task.activity}.compact
-      @tasks_medecine = @tasks.map{|task| task if task.medecine}.compact
-
-
-    # Permet de compter les tasks de différents type pour affiche le nombre
-    #dans la bulle rouge de la navbar
-    @tasks_food_type = Task.where.not(food_type_id: nil, doing: true, done: true)
-    @tasks_activity = Task.where.not(activity_id: nil, done: true)
-    @tasks_medecine = Task.where.not(medecine_id: nil, doing: true, done: true)
-
+      @tasks_food_type = Task.where.not(food_type_id: nil, doing: true, done: true)
+      @tasks_activity = Task.where.not(activity_id: nil, done: true)
+      @tasks_medecine = Task.where.not(medecine_id: nil, doing: true, done: true)
 
       users ||= User.all
       @owners = users.map{|user| user if user.role == "Propriétaire"}.compact
