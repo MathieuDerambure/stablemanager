ec266c63d84648679eaa79e801adeebc7e17f298
Mathieu Derambure <deramburemathieu@hotmail.com>
2018-09-05 12:24:52 +0200

    c'est mieux

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   def today
     DateTime.now
   end
-  
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :sur_name, :role, :phone, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
   end
 
 end
